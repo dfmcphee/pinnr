5a98b78666575e9b1e7c513ac72fdebc22606804
Thomas Kiperchuk <tkiperchuk@gmail.com>
2016-11-13 16:34:01 -0500

    Setting up login

--- server/routes.js
+++ server/routes.js
@@ -3,13 +3,33 @@ const index = require('./routes/index');
 const groups = require('./routes/groups');
 const posts = require('./routes/posts');
 const email = require('./routes/email');
+var passport = require('passport'),
+    signupController = require('../controllers/signupController.js')
+
+var isAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated())
+    return next()
+  req.flash('error', 'You have to be logged in to access the page.')
+  res.redirect('/')
+}
 
 const routes = function() {
+  router.get('/signup', signupController.show)
+  router.post('/signup', signupController.signup)
+  router.post('/login', passport.authenticate('local', {
+      successRedirect: '/',
+      failureRedirect: '/',
+      failureFlash: true
+  }))
+  router.get('/logout', function(req, res) {
+    req.logout()
+    res.redirect('/')
+  })
   router.get('/api/groups', groups.index);
-  router.post('/api/groups', groups.create);
+  router.post('/api/groups', isAuthenticated, groups.create);
   router.get('/api/posts', posts.index);
   router.post('/api/posts', posts.create);
-  router.delete('/api/posts/:id/', posts.delete);
+  router.delete('/api/posts/:id/', isAuthenticated, posts.delete);
   router.post('/api/email', email);
   router.get('*', index);
 
